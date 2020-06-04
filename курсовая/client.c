5dc4e764c5db2aaba38a2081e77c890888f0bcb8
Artem <48457398+Boyarushnikk@users.noreply.github.com>
2020-06-04 14:41:59 +0300

    Add files via upload

--- курсовая/client.c
+++ курсовая/client.c
@@ -1,94 +1,87 @@
 #include <stdio.h>
-#include <sys/shm.h>
 #include <stdlib.h>
-#include <pthread.h>
 #include <unistd.h>
-#include <sys/types.h>
-#include <sys/wait.h>
 #include <string.h>
-#include <fcntl.h>
-#include <sys/stat.h>
-#include <sys/sem.h>
-#include <sys/msg.h>
 #include <sys/ipc.h>
-#include <fcntl.h>
-#include <sys/mman.h>
-#include <sys/stat.h>
-#include <signal.h>
 #include <sys/socket.h>
+#include <sys/types.h>
 #include <netinet/in.h>
 #include <arpa/inet.h>
-#include <errno.h>
-#include <string>
-#include <iostream>
+#include <time.h>
 
-int main(int argc, char *argv[]) {
-	    int sockfd = 0, n = 0;
-	    char recvBuff[1024];
-	    struct sockaddr_in serv_addr;
-	    if(argc != 3) {
-	        printf("\n Usage: %s <ip of server> \n",argv[0]);
-	        return 1;
-	    }
-	    memset(recvBuff, '0',sizeof(recvBuff));
-	    if((sockfd = socket(AF_INET, SOCK_STREAM, 0)) < 0)
-	    {
-	        printf("\n Error : Could not create socket \n");
-	        return 1;
-	    }
-	    memset(&serv_addr, '0', sizeof(serv_addr));
-	    serv_addr.sin_family = AF_INET;
-	    serv_addr.sin_port = htons(atoi(argv[2]));
-	    if(inet_pton(AF_INET, argv[1], &serv_addr.sin_addr)<=0)
-	    {
-	        printf("\n inet_pton error occured\n");
-	        return 1;
-	    }
-	    if( connect(sockfd, (struct sockaddr *)&serv_addr, sizeof(serv_addr)) < 0) // подключение
-	    {
-	       printf("\n Error : Connect Failed \n");
-	       return 1;
-	    }
-        printf("Connection sucsess\n");
-        printf("Wait..\n");
-	while (1) {
-	int check;
-	read (sockfd, &check, sizeof(int)); // Читаем код от сервера
-	check = ntohs(check);
-	switch (check)
+int main(int argc, char *argv[])
+{
+	int sockfd = 0, n = 0;
+	char recvBuff[1024];
+	struct sockaddr_in serv_addr;
+	if (argc != 3)
 	{
-	case 0:
-	printf("Good\n"); // успешный ход
-	break;
-
-	case 1:
-	printf("Dead\n"); // смэрть
-	break;
-
-	case 2:
-	printf("Opponent give up\n"); // Противник сдался
+		printf("\n Usage: %s <ip of server> \n", argv[0]);
+		return 1;
+	}
+	memset(recvBuff, '0', sizeof(recvBuff));
+	if ((sockfd = socket(AF_INET, SOCK_STREAM, 0)) < 0)
+	{
+		printf("\n Error : Could not create socket \n");
+		return 1;
+	}
+	memset(&serv_addr, '0', sizeof(serv_addr));
+	serv_addr.sin_family = AF_INET;
+	serv_addr.sin_port = htons(atoi(argv[2]));
+	if (inet_pton(AF_INET, argv[1], &serv_addr.sin_addr) <= 0)
+	{
+		printf("\n inet_pton error occured\n");
+		return 1;
+	}
+	if (connect(sockfd, (struct sockaddr *)&serv_addr, sizeof(serv_addr)) < 0) // подключение
+	{
+		printf("\n Error : Connect Failed \n");
+		return 1;
+	}
+	printf("Connection sucsess\n");
 	printf("Wait..\n");
-	break;
-	
-	case 3:
-	printf("Opponent is dead\n"); // Противник умер
-	break;
+	while (1)
+	{
+		int check;
+		read(sockfd, &check, sizeof(int)); // Читаем код от сервера
+		check = ntohs(check);
+		switch (check)
+		{
+		case 0:
+			printf("Good\n"); // успешный ход
+			break;
 
-	case 4: // Наш ход
-	printf("Choose: 0-shoot, 1-out\n");
-	do {
-	std::cin >> check;
-	} while (check != 0 && check != 1); // Проверка ввода 0 или 1
-	if (check == 0) {
-		check = htons(check);
-		write (sockfd, &check, sizeof(int)); // отправляем ход
+		case 1:
+			printf("Dead\n"); // смэрть
+			break;
+
+		case 2:
+			printf("Opponent give up\n"); // Противник сдался
+			printf("Wait..\n");
+			break;
+
+		case 3:
+			printf("Opponent is dead\n"); // Противник умер
+			break;
+
+		case 4: // Наш ход
+			printf("Choose: 0-shoot, 1-out\n");
+			do
+			{
+				scanf("%d", &check);
+			} while (check != 0 && check != 1); // Проверка ввода 0 или 1
+			if (check == 0)
+			{
+				check = htons(check);
+				write(sockfd, &check, sizeof(int)); // отправляем ход
+			}
+			else
+			{
+				check = htons(check);
+				write(sockfd, &check, sizeof(int)); // отправляем ход и завершаем работу
+				return 0;
+			}
+			break;
+		}
 	}
-	else {
-		check = htons(check);
-		write (sockfd, &check, sizeof(int)); // отправляем ход и завершаем работу
-		return 0;
-	}
-	break;
-	}
-    }
 }
--- курсовая/server.c
+++ курсовая/server.c
@@ -1,132 +1,120 @@
 #include <stdio.h>
-#include <sys/shm.h>
 #include <stdlib.h>
-#include <pthread.h>
 #include <unistd.h>
-#include <sys/types.h>
-#include <sys/wait.h>
 #include <string.h>
-#include <fcntl.h>
-#include <sys/stat.h>
-#include <sys/sem.h>
-#include <sys/msg.h>
 #include <sys/ipc.h>
-#include <fcntl.h>
-#include <sys/mman.h>
-#include <sys/stat.h>
-#include <signal.h>
 #include <sys/socket.h>
+#include <sys/types.h>
 #include <netinet/in.h>
 #include <arpa/inet.h>
-#include <errno.h>
 #include <time.h>
-#include <string>
-#include <iostream>
-#include <ctime>
 
-void game(int listenfd, int cl1, int cl2) {
-    while (1) {
-	int num = 8;
-	srand(static_cast<unsigned int>(time(0)));
-	rand();
-	int rev[8];
-	int check;
-	for(int i=0; i<num; i++)
+void game(int listenfd, int cl1, int cl2)
+{
+	while (1)
 	{
-		rev[i]=0;
-	}
-	rev[rand()%num]=1;
-	for(int i=0; i<num; i++)
-	{
-		if (i%2==0) // ход 1 игрока
+		int num = 8;
+		srand(time(NULL));
+		rand();
+		int rev[8];
+		int check;
+		for (int i = 0; i < num; i++)
 		{
-			check = 4;
-   			check = htons (check);
-			write (cl1, &check, sizeof(int));
-			read (cl1, &check, sizeof(int));
-			check = ntohs (check);
-			if (check == 0) // ход
-			{
-				if(rev[i]==0) // успешный ход
-				{
-				check = 0;
-   				check = htons (check);
-				write (cl1, &check, sizeof(int));
-				}
-				else // смэрть
-				{
-				check = 1;
-				check = htons (check);
-				write (cl1, &check, sizeof(int));
-				check = 3;
-				check = htons (check);
-				write (cl2, &check, sizeof(int));
-				break;
-				}
-			}
-			else // сдался
-			{
-			check = 2;
-			check = htons (check);
-			write (cl2, &check, sizeof(int));
-			cl1 = accept(listenfd, (struct sockaddr*)NULL, NULL); //ждем подключеня 1 клиента
-			break;
-			}
+			rev[i] = 0;
 		}
-		else // аналогично для хода второго игрока
+		rev[rand() % num] = 1;
+		for (int i = 0; i < num; i++)
 		{
-			check = 4;
-   			check = htons (check);
-			write (cl2, &check, sizeof(int));
-			read (cl2, &check, sizeof(int));
-			check = ntohs (check);
-			if (check == 0)
+			if (i % 2 == 0) // ход 1 игрока
 			{
-				if(rev[i]==0)
+				check = 4;
+				check = htons(check);
+				write(cl1, &check, sizeof(int));
+				read(cl1, &check, sizeof(int));
+				check = ntohs(check);
+				if (check == 0) // ход
 				{
-				check = 0;
-   				check = htons (check);
-				write (cl2, &check, sizeof(int));
+					if (rev[i] == 0) // успешный ход
+					{
+						check = 0;
+						check = htons(check);
+						write(cl1, &check, sizeof(int));
+					}
+					else // смэрть
+					{
+						check = 1;
+						check = htons(check);
+						write(cl1, &check, sizeof(int));
+						check = 3;
+						check = htons(check);
+						write(cl2, &check, sizeof(int));
+						break;
+					}
+				}
+				else // сдался
+				{
+					check = 2;
+					check = htons(check);
+					write(cl2, &check, sizeof(int));
+					cl1 = accept(listenfd, (struct sockaddr *)NULL, NULL); //ждем подключеня 1 клиента
+					break;
+				}
+			}
+			else // аналогично для хода второго игрока
+			{
+				check = 4;
+				check = htons(check);
+				write(cl2, &check, sizeof(int));
+				read(cl2, &check, sizeof(int));
+				check = ntohs(check);
+				if (check == 0)
+				{
+					if (rev[i] == 0)
+					{
+						check = 0;
+						check = htons(check);
+						write(cl2, &check, sizeof(int));
+					}
+					else
+					{
+						check = 1;
+						check = htons(check);
+						write(cl2, &check, sizeof(int));
+						check = 3;
+						check = htons(check);
+						write(cl1, &check, sizeof(int));
+						break;
+					}
 				}
 				else
 				{
-				check = 1;
-				check = htons (check);
-				write (cl2, &check, sizeof(int));
-				check = 3;
-				check = htons (check);
-				write (cl1, &check, sizeof(int));
-				break;
+					check = 2;
+					check = htons(check);
+					write(cl1, &check, sizeof(int));
+					cl2 = accept(listenfd, (struct sockaddr *)NULL, NULL); //ждем подключеня 2 клиента
+					break;
 				}
-			}
-			else
-			{
-			check = 2;
-			check = htons (check);
-			write (cl1, &check, sizeof(int));
-			cl2 = accept(listenfd, (struct sockaddr*)NULL, NULL); //ждем подключеня 2 клиента
-			break;
 			}
 		}
 	}
-    }
 }
 
-int main(int argc, char *argv[]) {
-	    int listenfd = 0;
-	    struct sockaddr_in serv_addr;
-	    char sendBuff[1025];
-	    time_t ticks;
-	    listenfd = socket(AF_INET, SOCK_STREAM, 0);
-	    memset(&serv_addr, '0', sizeof(serv_addr));
-	    memset(sendBuff, '0', sizeof(sendBuff));
+int main(int argc, char *argv[])
+{
+	int listenfd = 0;
+	struct sockaddr_in serv_addr;
+	char sendBuff[1025];
+	time_t ticks;
+	listenfd = socket(AF_INET, SOCK_STREAM, 0);
+	memset(&serv_addr, '0', sizeof(serv_addr));
+	memset(sendBuff, '0', sizeof(sendBuff));
 
-	    serv_addr.sin_family = AF_INET;
-	    serv_addr.sin_addr.s_addr = htonl(INADDR_ANY);
-	    serv_addr.sin_port = htons(atoi(argv[1]));
-	    bind(listenfd, (struct sockaddr*)&serv_addr, sizeof(serv_addr));
-	    listen(listenfd, 10);
-	int cl1 = accept(listenfd, (struct sockaddr*)NULL, NULL); //ждем подключеня 1 клиента
-    	int cl2 = accept(listenfd, (struct sockaddr*)NULL, NULL); //ждем подключеня 2 клиента
-        game(listenfd, cl1, cl2);
+	serv_addr.sin_family = AF_INET;
+	serv_addr.sin_addr.s_addr = htonl(INADDR_ANY);
+	serv_addr.sin_port = htons(atoi(argv[1]));
+	bind(listenfd, (struct sockaddr *)&serv_addr, sizeof(serv_addr));
+	listen(listenfd, 10);
+	int cl1 = accept(listenfd, (struct sockaddr *)NULL, NULL); //ждем подключеня 1 клиента
+	int cl2 = accept(listenfd, (struct sockaddr *)NULL, NULL); //ждем подключеня 2 клиента
+	game(listenfd, cl1, cl2);
 }
